6b69af0c3ebc87bf78b928c60d9b0d870159bd18
ThaboChauke <chaukethabo16@gmail.com>
2024-11-21 12:56:59 +0200

    send response

--- src/main/java/com/chauke/SimpleHTTPServer.java
+++ src/main/java/com/chauke/SimpleHTTPServer.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
 
 public class SimpleHTTPServer {
 
@@ -14,17 +16,27 @@ public class SimpleHTTPServer {
         final ServerSocket server = new ServerSocket(PORT);
         System.out.println("Listening for connection on port " + PORT + "...");
 
+//        while (true) {
+//            Socket clientSocket = server.accept();
+//            InputStreamReader inputStreamReader = new InputStreamReader(clientSocket.getInputStream());
+//            BufferedReader reader = new BufferedReader(inputStreamReader);
+//
+//            String line = reader.readLine();
+//
+//            while (!line.isEmpty()) {
+//                System.out.println(line);
+//                line = reader.readLine();
+//            }
+//        }
+
         while (true) {
-            Socket clientSocket = server.accept();
-            InputStreamReader inputStreamReader = new InputStreamReader(clientSocket.getInputStream());
-            BufferedReader reader = new BufferedReader(inputStreamReader);
-
-            String line = reader.readLine();
-
-            while (!line.isEmpty()) {
-                System.out.println(line);
-                line = reader.readLine();
+            try (Socket clientSocket = server.accept()) {
+                Date today = new Date();
+                String httpResponse = "HTTP/1.1 200 OK\r\n\r\n" + today;
+                clientSocket.getOutputStream().write(httpResponse.getBytes(StandardCharsets.UTF_8));
             }
+
+
         }
     }
 }
