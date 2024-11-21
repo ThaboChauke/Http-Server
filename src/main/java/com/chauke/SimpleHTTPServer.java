b4d326be2ec35bbeab1dd04452dbed23001ba1d5
ThaboChauke <chaukethabo16@gmail.com>
2024-11-21 11:03:26 +0200

    accept connections

--- src/main/java/com/chauke/SimpleHTTPServer.java
+++ src/main/java/com/chauke/SimpleHTTPServer.java
@@ -1,7 +1,10 @@
 package com.chauke;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class SimpleHTTPServer {
 
@@ -10,8 +13,18 @@ public class SimpleHTTPServer {
         final int PORT = 8080;
         final ServerSocket server = new ServerSocket(PORT);
         System.out.println("Listening for connection on port " + PORT + "...");
-        while (true) {
 
+        while (true) {
+            Socket clientSocket = server.accept();
+            InputStreamReader inputStreamReader = new InputStreamReader(clientSocket.getInputStream());
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+
+            String line = reader.readLine();
+
+            while (!line.isEmpty()) {
+                System.out.println(line);
+                line = reader.readLine();
+            }
         }
     }
 }
